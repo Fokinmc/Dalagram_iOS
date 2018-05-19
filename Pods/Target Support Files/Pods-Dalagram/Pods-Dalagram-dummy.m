1a2697829925605331dc9a5e1e9bf0fee6521456
Toremurat <zholayev.t@gmail.com>
2018-05-19 18:55:50 +0600

    Test 1

--- Pods/Target Support Files/Pods-Dalagram/Pods-Dalagram-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Dalagram : NSObject
-@end
-@implementation PodsDummy_Pods_Dalagram
-@end
--- Pods/Target Support Files/Pods-DalagramTests/Pods-DalagramTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DalagramTests : NSObject
-@end
-@implementation PodsDummy_Pods_DalagramTests
-@end
